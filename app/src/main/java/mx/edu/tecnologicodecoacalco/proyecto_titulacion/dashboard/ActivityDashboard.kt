e056e166bb0c69379cc2ac67139579a05068e02e
Rogelio Martinez <roger4567.rm@gmail.com>
2022-01-28 02:16:39 -0600

    [RogMG][Improvement] Added fragments to buttom view bar

--- app/src/main/java/mx/edu/tecnologicodecoacalco/proyecto_titulacion/dashboard/ActivityDashboard.kt
+++ app/src/main/java/mx/edu/tecnologicodecoacalco/proyecto_titulacion/dashboard/ActivityDashboard.kt
@@ -4,7 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import mx.edu.tecnologicodecoacalco.proyecto_titulacion.R
 import mx.edu.tecnologicodecoacalco.proyecto_titulacion.databinding.ActivityDashboardBinding
 
@@ -23,11 +24,48 @@ class ActivityDashboard : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
-
-        binding.bottomNavigationBar.background = null;
+        setContentView(binding.root)
+        binding.bottomNavigationBar.background = null
         binding.bottomNavigationBar.menu.getItem(2).isEnabled = false;
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.monitor_fragment -> {
+                    replaceFragmentView(MonitorFragment())
+                    true
+                }
+                R.id.app_conection_fragment -> {
+                    replaceFragmentView(AppConectionFragment())
+                    true
+                }
+                R.id.advices_fragment -> {
+                    replaceFragmentView(AdvicesFragment())
+                    true
+                }
+                R.id.settings_fragment -> {
+                    replaceFragmentView(SettingsFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+        binding.fab.setOnClickListener {
+            replaceFragmentView(BabyRegisterFragment())
+        }
+
+    }
 
 
+    fun replaceFragmentView(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.containerViewsFragment.id, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.setReorderingAllowed(false)
+        if(fragmentManager.backStackEntryCount > 3) {
+            fragmentManager.popBackStack()
+        }
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/mx/edu/tecnologicodecoacalco/proyecto_titulacion/dashboard/AdvicesFragment.kt
@@ -0,0 +1,35 @@
+package mx.edu.tecnologicodecoacalco.proyecto_titulacion.dashboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.edu.tecnologicodecoacalco.proyecto_titulacion.R
+import mx.edu.tecnologicodecoacalco.proyecto_titulacion.databinding.FragmentAdvicesBinding
+import mx.edu.tecnologicodecoacalco.proyecto_titulacion.databinding.FragmentSettingsBinding
+
+
+class AdvicesFragment : Fragment() {
+
+    private var _binding: FragmentAdvicesBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAdvicesBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
