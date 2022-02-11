14305205a9998a205f40ed7b771ae2ef54936802
VALO66693 <96094312+VALO66693@users.noreply.github.com>
2022-02-10 18:44:58 -0600

    [valo][pantallas]

---
+++ app/src/main/java/mx/edu/tecnologicodecoacalco/proyecto_titulacion/dashboard/advices/presentation/view/Advices_Description.kt
@@ -0,0 +1,12 @@
+package mx.edu.tecnologicodecoacalco.proyecto_titulacion.dashboard.advices.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.tecnologicodecoacalco.proyecto_titulacion.R
+
+class Advices_Description : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advices_description)
+    }
+}
